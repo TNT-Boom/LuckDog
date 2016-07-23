9fccf552fdf74867fd640f38d6f8063235d7b794
LiMingjian <838256018@qq.com>
2016-07-23 23:51:04 +0800

    增加JVM学习教程

---
+++ JVMLearn/src/com/limingjian/jvmlearn/JvmLearn.java
@@ -0,0 +1,12 @@
+package com.limingjian.jvmlearn;
+
+public class JvmLearn
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		JHeapOutMem.OutOfMem();
+	}
+
+}
