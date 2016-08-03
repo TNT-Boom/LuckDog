ef6128c01a1dbbe49a16ef03bb802573c17a4a9d
LiMingjian <838256018@qq.com>
2016-08-03 15:55:35 +0800

    由子类决定如何实现创建对象的逻辑，父类不在管。

--- DesignerPattern/src/com/limingjian/factory/PizzaStore.java
+++ DesignerPattern/src/com/limingjian/factory/PizzaStore.java
@@ -1,22 +1,17 @@
 package com.limingjian.factory;
 
-public class PizzaStore
+public abstract class PizzaStore
 {
-	SimplePizzaFactory factory;
-	
-	public PizzaStore(SimplePizzaFactory factory)
-	{
-		this.factory = factory;
-	}
 	
 	public Pizza orderPizza(String type)
 	{
 		Pizza pizza = null;
-		pizza = factory.createPizza(type);
+		pizza = createPizza(type);
 		pizza.prepare();
 		pizza.bake();
 		pizza.cut();
 		pizza.box();
 		return pizza;
 	}
+	public abstract Pizza createPizza(String type);
 }
