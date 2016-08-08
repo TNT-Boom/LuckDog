f567e2a4085e743d80f6657ef09732eda219491a
LiMingjian <838256018@qq.com>
2016-08-08 20:28:52 +0800

    增加netty5服务端

---
+++ Netty5Hello/src/com/limingjian/server/MyChannelInitializer.java
@@ -0,0 +1,18 @@
+package com.limingjian.server;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class MyChannelInitializer extends ChannelInitializer<Channel>
+{
+
+	@Override
+	protected void initChannel(Channel ch)
+		throws Exception
+	{
+		ch.pipeline().addLast(new StringDecoder(), new StringEncoder(), new ServerHandler());
+	}
+
+}
