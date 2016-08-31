5de0e5ece8696f3ea38e3cf8a7e4128b92d4e99e
LiMingjian <838256018@qq.com>
2016-08-31 16:42:50 +0800

    增加排列组合代码

---
+++ Treasure/src/leetcode/zuochengyun/pailiezuhe/PaiLieZuheTools.java
@@ -0,0 +1,42 @@
+package leetcode.zuochengyun.pailiezuhe;
+
+public class PaiLieZuheTools
+{
+	public static void main(String[] args)
+	{
+		System.out.println(cnm(4, 2));
+		System.out.println(anm(3, 2));
+		System.out.println(ajiecheng(3));
+	}
+
+	public static int cnm(int n, int m)
+	{
+		int count = m;
+		int fenzi = 1;
+		int fenmu = 1;
+		while (count > 0)
+		{
+			fenzi *= (n - count + 1);
+			fenmu *= count--;
+		}
+		return fenzi / fenmu;
+	}
+
+	public static int anm(int n, int m)
+	{
+		int count = m;
+		int total = 1;
+		;
+		while (count > 0)
+			total *= (n - count-- + 1);
+		return total;
+	}
+
+	public static int ajiecheng(int n)
+	{
+		int total = 1;
+		while (n > 0)
+			total *= (n--);
+		return total;
+	}
+}
