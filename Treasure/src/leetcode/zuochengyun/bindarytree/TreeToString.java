80590ffd593fa7bb6bc4a9a870d2460c1e43a75d
LiMingjian <838256018@qq.com>
2016-09-02 17:04:19 +0800

    增加二叉树代码

---
+++ Treasure/src/leetcode/zuochengyun/bindarytree/TreeToString.java
@@ -0,0 +1,34 @@
+package leetcode.zuochengyun.bindarytree;
+
+public class TreeToString
+{
+	public class TreeNode
+	{
+		int val = 0;
+		TreeNode left = null;
+		TreeNode right = null;
+
+		public TreeNode(int val)
+		{
+			this.val = val;
+		}
+	}
+
+	public String toString(TreeNode root)
+	{
+		StringBuilder builder = new StringBuilder();
+		Serialize(root, builder);
+		return builder.toString();
+	}
+	private void Serialize(TreeNode root, StringBuilder builder)
+	{
+		if(root == null)
+			builder.append("#!");
+		else
+		{
+			builder.append(root.val).append("!");
+			Serialize(root.left, builder);
+			Serialize(root.right, builder);
+		}
+	}
+}
