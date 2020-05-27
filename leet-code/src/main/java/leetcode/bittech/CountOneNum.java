fbdce5aada3f55cb42435b071ec0caa10a2cae11
limingjian <limingjian@yy.com>
2020-05-27 20:28:35 +0800

    计算二进制中1的个数

--- leet-code/src/main/java/leetcode/bittech/CountOneNum.java
+++ leet-code/src/main/java/leetcode/bittech/CountOneNum.java
@@ -4,7 +4,17 @@ package leetcode.bittech;
  * @author limingjian
  * @date 2020/4/12
  * 计算一个数中二进制1的个数
- * x ^ (x - 1)，本质就是将最右一个1变为0
+ * x & (x - 1)，本质就是将最右一个1变为0
  */
 public class CountOneNum {
+
+    public int count(int x) {
+        int count = 0;
+        while (x != 0) {
+            x = x & (x - 1);
+            count++;
+        }
+
+        return count;
+    }
 }
